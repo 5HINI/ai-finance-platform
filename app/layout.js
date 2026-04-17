ee062957c0c40c497c34da837cd5eca4d07503fe
5HINI <asghetyt@gmail.com>
2026-04-17 08:22:14 +0530

    Deploy

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "A5L",
+  title: "RupeeX",
   description: "One stop Finance Platform",
 };
 
